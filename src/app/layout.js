623d407bb700929c88ba2723eaf2c211c5dae2be
Hamza Sheikh <zafarhamza789@gmail.com>
2025-04-12 18:42:52 +0500

    sidebar_36Mint

---
+++ src/app/layout.js
@@ -0,0 +1,27 @@
+import { Inter} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata = {
+  title: "DeepSeek - WebAPP",
+  description: "Full Stack App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+    <body className={`${inter.className} antialiased`}>
+      {children}
+    </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,45 @@
+'use client';
+import Image from "next/image";
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+
+export default function Home() {
+
+  const [expand, setExpand] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <div>
+      <div className="flex h-screen">
+        {/* sidebar */}
+        <div className="flex flex-1 flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
+          <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
+            <Image className="rotate-180"
+              src={assets.menu_icon}
+              alt=""
+              onClick={() => (expand ? setExpand(false) : setExpand(true))} />
+            <Image className="opacity-70" src={assets.chat_icon} alt="" />
+          </div>
+
+          {messages.length === 0 ? (
+            <>
+              <div className="flex items-center gap-3">
+                <Image className="h-16" src={assets.logo_icon} alt="" />
+                <p className="text-2xl font-medium">HI, I am DeepSeek</p>
+              </div>
+
+              <p className="text-sm mt-2">How can i help you today</p>
+            </>
+          ) : (<div></div>)}
+
+          {/* prompt-box */}
+
+          <p className="text-xs absolute bottom-1 text-gray-500">AI-Generative for reference only</p>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
