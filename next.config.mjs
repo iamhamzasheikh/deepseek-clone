6902acf4b1d6ea38b645ca21bac6e7444e417d4c
Hamza Sheikh <zafarhamza789@gmail.com>
2025-04-26 21:46:23 +0500

    oka

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,32 @@
 // export default nextConfig;
 
 // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     devIndicators: {
+//         buildActivity: false,
+//     },
+// }
+
+// export default nextConfig;
+
+
+// next.config.mjs
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     devIndicators: {
         buildActivity: false,
+    },
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.resolve(__dirname);
+        return config;
     },
 }
 
